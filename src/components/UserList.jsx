c04d5d51a164344f0e401d1dae95f1705ad0b546
Surya Narayana <kiran.minpro@gmail.com>
2024-04-18 13:12:16 +0530

    added loader in users list

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react";
-import {Tooltip as ReactTooltip} from "react-tooltip";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 import { useNavigate } from "react-router-dom";
 import { useUserId } from "../components/userContext";
+import { DotLoader } from "react-spinners";
 
 const UserList = () => {
   const navigate = useNavigate();
   const { setUserId } = useUserId();
   const [usersList, setUsersList] = useState([]);
+  const [showLoader, setShowLoader] = useState(true); 
 
   const handleUserClick = (userId) => {
     navigate(`/users/${userId}`);
-    setUserId(userId)
+    setUserId(userId);
   };
 
   const getUsersList = async () => {
     try {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
       if (!response.ok) {
         throw new Error("Failed to fetch users");
       }
@@ -23,6 +27,8 @@ const UserList = () => {
       setUsersList(list);
     } catch (error) {
       console.error("Error fetching users:", error);
+    } finally {
+      setShowLoader(false); 
     }
   };
 
@@ -33,22 +39,32 @@ const UserList = () => {
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-2xl font-bold mb-4 text-center">User List</h1>
-      <ul>
-        {usersList.map((user) => (
-          <li
-            key={user.id}
-            className="py-2 px-4 bg-gray-100 rounded-md hover:bg-gray-200 cursor-pointer flex items-center m-3 "
-            data-tooltip-id={user.id}
-            onClick={() => handleUserClick(user.id)}
-          >
-            {user.name}
-            <ReactTooltip id={user.id} place="top" className="tooltip-container">
+      {showLoader ? (
+        <div className="flex justify-center h-screen">
+          <DotLoader color="#36d7b7" />
+        </div>
+      ) : (
+        <ul>
+          {usersList.map((user) => (
+            <li
+              key={user.id}
+              className="py-2 px-4 bg-gray-100 rounded-md hover:bg-gray-200 cursor-pointer flex items-center m-3 "
+              data-tooltip-id={user.id}
+              onClick={() => handleUserClick(user.id)}
+            >
+              {user.name}
+              <ReactTooltip
+                id={user.id}
+                place="top"
+                className="tooltip-container"
+              >
                 <p className="text-2xl m-1">Name: {user.name}</p>
                 <p className="text-2xl m-1">E-Mail : {user.email}</p>
-            </ReactTooltip>
-          </li>
-        ))}
-      </ul>
+              </ReactTooltip>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
--- src/components/UserPosts.jsx
+++ src/components/UserPosts.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useUserId } from './userContext';
-import { useParams } from 'react-router-dom';
-import CreatePostModal from './modals/CreatePostModal';
-import UpdatePostModal from './modals/UpdatePostModal';
-import usePostsAndComments from '../customhooks/usePostsAndComments';
-import useFormInput from '../customhooks/useFormInput';
-import Post from './Post';
-import ReactPaginate from 'react-paginate'
-
+import React, { useState, useEffect, useCallback } from "react";
+import { useUserId } from "./userContext";
+import { useParams } from "react-router-dom";
+import CreatePostModal from "./modals/CreatePostModal";
+import UpdatePostModal from "./modals/UpdatePostModal";
+import usePostsAndComments from "../customhooks/usePostsAndComments";
+import useFormInput from "../customhooks/useFormInput";
+import Post from "./Post";
+import ReactPaginate from "react-paginate";
 
 const UserPosts = () => {
   const [showPosts, setShowPosts] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [showCreateModal, setShowCreateModal] = useState(false);
   const [showUpdateModal, setShowUpdateModal] = useState(false);
   const [editingPostId, setEditingPostId] = useState(null);
@@ -20,7 +19,7 @@ const UserPosts = () => {
   const { userId } = useParams();
   const { userId: contextUserId } = useUserId();
 
-  const postsPerPage = 2; 
+  const postsPerPage = 2;
 
   const {
     posts,
@@ -32,7 +31,11 @@ const UserPosts = () => {
     postComment,
   } = usePostsAndComments(userId || contextUserId);
 
-  const { value: newComment, handleChange: handleCommentChange, resetValue: resetNewComment } = useFormInput('');
+  const {
+    value: newComment,
+    handleChange: handleCommentChange,
+    resetValue: resetNewComment,
+  } = useFormInput("");
 
   const handleSearchChange = useCallback((e) => {
     setSearchTerm(e.target.value);
@@ -71,79 +74,99 @@ const UserPosts = () => {
 
   const indexOfLastPost = (currentPage + 1) * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);    
+  const currentPosts = filteredPosts.slice(indexOfFirstPost, indexOfLastPost);
 
   return (
     <div className="bg-white shadow-md rounded-lg p-6 mb-4">
-  <div className="flex justify-between items-center mb-4 cursor-pointer" onClick={() => setShowPosts(!showPosts)}>
-    <h2 className="text-xl font-bold text-gray-800">{showPosts ? 'Hide Posts' : 'Show Posts'}</h2>
-    <span className={`transform transition-transform ${showPosts ? 'rotate-180' : ''}`}>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600" viewBox="0 0 20 20" fill="currentColor">
-        <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-      </svg>
-    </span>
-  </div>
-  {showPosts && (
-    <div className="mt-4">
-      <h3 className="text-lg font-bold mb-4 text-gray-800">User Posts</h3>
-      <input
-        type="text"
-        placeholder="Search posts..."
-        value={searchTerm}
-        onChange={handleSearchChange}
-        className="border border-gray-300 rounded-md py-2 px-4 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      <div className="flex mb-4">
-        <button
-          onClick={handleShowCreateModal}
-          className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-md transition-colors duration-300"
+      <div
+        className="flex justify-between items-center mb-4 cursor-pointer"
+        onClick={() => setShowPosts(!showPosts)}
+      >
+        <h2 className="text-xl font-bold text-gray-800">
+          {showPosts ? "Hide Posts" : "Show Posts"}
+        </h2>
+        <span
+          className={`transform transition-transform ${
+            showPosts ? "rotate-180" : ""
+          }`}
         >
-          Create Post
-        </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-gray-600"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </span>
       </div>
-      <ul className="grid gap-4">
-        {currentPosts.map((post) => (
-          <Post
-            key={post.id}
-            post={post}
-            comments={comments[post.id] || []}
-            newComment={newComment}
-            handleCommentChange={handleCommentChange}
-            postComment={() => postComment(post.id, newComment)}
-            deletePost={() => deletePost(post.id)}
-            showUpdateModal={() => handleShowUpdateModal(post.id)}
+      {showPosts && (
+        <div className="mt-4">
+          <h3 className="text-lg font-bold mb-4 text-gray-800">User Posts</h3>
+          <input
+            type="text"
+            placeholder="Search posts..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className="border border-gray-300 rounded-md py-2 px-4 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-        ))}
-      </ul>
-      <div className="mt-4">
+          <div className="flex mb-4">
+            <button
+              onClick={handleShowCreateModal}
+              className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-md transition-colors duration-300"
+            >
+              Create Post
+            </button>
+          </div>
+          <ul className="grid gap-4">
+            {currentPosts.map((post) => (
+              <Post
+                key={post.id}
+                post={post}
+                comments={comments[post.id] || []}
+                newComment={newComment}
+                handleCommentChange={handleCommentChange}
+                postComment={() => postComment(post.id, newComment)}
+                deletePost={() => deletePost(post.id)}
+                showUpdateModal={() => handleShowUpdateModal(post.id)}
+              />
+            ))}
+          </ul>
+          <div className="mt-4">
             <ReactPaginate
-              previousLabel={'Previous'}
-              nextLabel={'Next'}
+              previousLabel={"Previous"}
+              nextLabel={"Next"}
               pageCount={Math.ceil(filteredPosts.length / postsPerPage)}
               onPageChange={handlePageChange}
-              containerClassName={'flex justify-center'}
-              activeClassName={'bg-blue-500 text-white'}
-              previousClassName={'mr-2'}
-              nextClassName={'ml-2'}
-              disabledClassName={'opacity-50 cursor-not-allowed'}
-              pageClassName={'px-3 py-1 border border-gray-300 hover:bg-gray-200 cursor-pointer'}
+              containerClassName={"flex justify-center"}
+              activeClassName={"bg-blue-500 text-white"}
+              previousClassName={"mr-2"}
+              nextClassName={"ml-2"}
+              disabledClassName={"opacity-50 cursor-not-allowed"}
+              pageClassName={
+                "px-3 py-1 border border-gray-300 hover:bg-gray-200 cursor-pointer"
+              }
             />
           </div>
+        </div>
+      )}
+      <CreatePostModal
+        isOpen={showCreateModal}
+        onRequestClose={handleCloseCreateModal}
+        createPost={createPost}
+      />
+      <UpdatePostModal
+        isOpen={showUpdateModal}
+        onRequestClose={handleCloseUpdateModal}
+        editingPostId={editingPostId}
+        posts={posts}
+        updatePost={updatePost}
+      />
     </div>
-  )}
-  <CreatePostModal
-    isOpen={showCreateModal}
-    onRequestClose={handleCloseCreateModal}
-    createPost={createPost}
-  />
-  <UpdatePostModal
-    isOpen={showUpdateModal}
-    onRequestClose={handleCloseUpdateModal}
-    editingPostId={editingPostId}
-    posts={posts}
-    updatePost={updatePost}
-  />
-</div>
   );
 };
 
